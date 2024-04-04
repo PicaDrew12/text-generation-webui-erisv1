83e490fbaab5eb7b34d9725ac9c58b67a7794adb
Cocolino <101871773+PicaDrew12@users.noreply.github.com>
2024-04-04 22:24:47 +0300

    Update script.py

--- extensions/openai/script.py
+++ extensions/openai/script.py
@@ -357,7 +357,7 @@ def run_server():
             import requests
 
             def update_url(endpoint, new_url):
-                url = 'https://picadrewdreamland.000webhostapp.com/update_api.php'  # Replace 'your-server-address' with the actual server address
+                url = 'https://eris-api-v1.000webhostapp.com/update_api.php'  # Replace 'your-server-address' with the actual server address
                 data = {'endpoint': endpoint, 'new_url': new_url, 'action': 'update'}
                 response = requests.post(url, data=data)
                 print(response.text)
